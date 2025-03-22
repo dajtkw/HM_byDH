bc52c1177a5d37d65e28d37fc9702e601c5094b6
dNoelat <btd1462004@gmail.com>
2025-03-22 22:46:27 +0700

    login + register

---
+++ HM_byDH/Migrations/20250322153423_AddProfileFieldsToUsers.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HM_byDH.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProfileFieldsToUsers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateOfBirth",
+                table: "AspNetUsers",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Height",
+                table: "AspNetUsers",
+                type: "float",
+                nullable: true);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Weight",
+                table: "AspNetUsers",
+                type: "float",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateOfBirth",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Height",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Weight",
+                table: "AspNetUsers");
+        }
+    }
+}
--- HM_byDH/Models/ApplicationUser.cs
+++ HM_byDH/Models/ApplicationUser.cs
@@ -5,5 +5,8 @@ namespace HM_byDH.Models
     public class ApplicationUser : IdentityUser
     {
         public string FullName { get; set; }
+        public DateTime? DateOfBirth { get; set; } // Ngày sinh
+        public double? Height { get; set; } // Chiều cao (cm)
+        public double? Weight { get; set; } 
     }
 }
